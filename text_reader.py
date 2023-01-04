aad57d3a9898d4f81c019410d6c37ae0fe6999e5
aaaaaaaaaaaaaahhh <blewconklin0264@student.pps.net>
2023-01-04 10:34:23 -0800

    added new venv

--- text_reader.py
+++ text_reader.py
@@ -2,8 +2,16 @@ import easyocr
 import numpy as np
 import torch
 
-img = 'register-car-florida-image-1166.jpg'
+img = '1.jpg'
 
 reader = easyocr.Reader(['en'], gpu=False)
+result = reader.readtext(img)
+
 
 model = torch.hub.load('yolov7', 'custom', source='local', path='best.pt')
+
+results = model(img)
+
+labels, cordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+
+print(results)
