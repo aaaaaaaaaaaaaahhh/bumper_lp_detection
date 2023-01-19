4d1f361eda4908a87bd62ad61c90a0193749149a
aaaaaaaaaaaaaahhh <blewconklin0264@student.pps.net>
2023-01-19 08:47:57 -0800

    api just is better than whatever we have don

--- yolov7/text_reader.py
+++ yolov7/text_reader.py
@@ -3,53 +3,27 @@ import cv2
 import numpy as np
 from detect import detect
 
-img = '1.jpg'
-
-reader = easyocr.Reader(['en'], gpu=False)
+img = '7.jpg'
 
 
-x1, x2, y1, y2 = detect(img, 'best.pt', 640, 0.4, 0, 'cpu', view_img=False)
-cv_img = cv2.imread(img)
 
-plate_img = cv_img[y1:y2, x1:x2]
-#cv2.imwrite(save_path, im0)
+import requests
+from pprint import pprint
+regions = ['us'] # Change to your country
+with open(img, 'rb') as fp:
+    response = requests.post(
+        'https://api.platerecognizer.com/v1/plate-reader/',
+        data=dict(regions=regions),  # Optional
+        files=dict(upload=fp),
+        headers={'Authorization': 'Token 2eabc8718a3cf43303e8dfe7d1e5fb9194dfd335'})
+pprint(response.json())
 
-
-# image cleaning
-
-def grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-gray_image = grayscale(plate_img)
-
-img_blur = cv2.GaussianBlur(gray_image, (3,3), 0) 
-
-# Canny Edge Detection
-edges = cv2.Canny(image=gray_image, threshold1=100, threshold2=200) # Canny Edge Detection
-# Display Canny Edge Detection Image
-
-
-thresh = cv2.threshold(edges, 128, 255, cv2.THRESH_BINARY)[1]
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-#contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#contours = contours[0] if len(contours) == 2 else contours[1]
-#big_contour = max(contours, key=cv2.contourArea)
-
-
-result = np.zeros_like(edges)
-cv2.drawContours(result, contours[5], -1, (255,255,255), cv2.FILLED)
-
-cv2.imshow('result', result)
-cv2.imshow('Canny Edge Detection', edges)
-cv2.waitKey(0)
-
-results = reader.readtext(plate_img)
-#xmin, xmax, ymin, ymax = results[0][0][0]
-#cropped_img = gray_image[xmin:xmax, ymin:ymax]
-print(results)
-
-#cv2.imshow('image', gray_image)
-#cv2.imshow('image', cropped_img)
-#cv2.waitKey(0)
-
+# Calling with a custom engine configuration
+import json
+with open(img, 'rb') as fp:
+    response = requests.post(
+        'https://api.platerecognizer.com/v1/plate-reader/',
+        data=dict(regions=['us'], config=json.dumps(dict(region="strict"))),  # Optional
+        files=dict(upload=fp),
+        headers={'Authorization': 'Token 2eabc8718a3cf43303e8dfe7d1e5fb9194dfd335'})
 
